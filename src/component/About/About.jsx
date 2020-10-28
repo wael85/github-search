e949d31a8f2a9aae313f96f71d8d9396d5815f49
Wael85 <dr.warl85@gmail.com>
2020-10-28 13:56:35 +0100

    add and style about page

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import './About.style.css';
+
  export function About (){
+   
      return(
-       <div>
-           About
+       <div className="about-block">
+           <p className="about-section">
+           This app  hellp to search GitHub users and display all there info and repos
+             the work done in tow part:
+             first part as a homework to hack your futuer 
+             secound as a project to school
+             for more information please contact me on 29920518
+           </p> 
+           <img className="random-img" src="https://picsum.photos/400/500" alt="wael" srcset=""/>
        </div>
      )
  }
